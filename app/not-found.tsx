96d2850ab7edc15153e03f805f682670823f0f0a
Jayant Hegde Kageri <jayantkageri@gmail.com>
2025-05-19 10:12:03 +0530

    Loading Component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -15,8 +15,8 @@ export default function NotFound() {
                 className="w-6 h-6 stroke-2 stroke-current"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
                 />
               </svg>
